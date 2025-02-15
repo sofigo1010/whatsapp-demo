12649be2615f4e40f1f5a40d6d97ed5cc062da34
sofia g <110646163+sofigo1010@users.noreply.github.com>
2025-02-15 01:10:29 -0600

    express

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.json()); // Para recibir datos en JSON
+app.use(express.json()); // Para recibir JSON
 
 app.post('/webhooks/orders/create', (req, res) => {
     console.log('📦 ¡Nuevo pedido recibido!', req.body);
-    res.sendStatus(200); // Responde a Shopify para confirmar la recepción
+    res.sendStatus(200); // Responder con 200 para que Shopify no reintente
 });
 
-app.listen(3000, () => console.log('🚀 Servidor corriendo en el puerto 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Servidor corriendo en el puerto ${PORT}`));
